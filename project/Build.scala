b7833489400871245449586fcd2d8926126f262d
Raphael Brand <raphaelbrand@gmail.com>
2013-10-25 20:18:53 +0200

    add mockito to dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,10 @@
 import sbt._
-import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "TvShowOrganizer"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "TvShowOrganizer"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -14,7 +13,8 @@ object ApplicationBuild extends Build {
     javaEbean,
     "mysql" % "mysql-connector-java" % "5.1.25",
     "org.apache.httpcomponents" % "httpcomponents-client" % "4.2.5",
-    "org.mindrot" % "jbcrypt" % "0.3m"
+    "org.mindrot" % "jbcrypt" % "0.3m",
+    "org.mockito" % "mockito-all" % "1.9.0"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
