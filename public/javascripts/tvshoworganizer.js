0ca260b9c68ed16ae1440b088aa15ad444579fe5
Raphael Brand <raphaelbrand@gmail.com>
2013-11-02 15:56:40 +0100

    search on enter key press

--- public/javascripts/tvshoworganizer.js
+++ public/javascripts/tvshoworganizer.js
@@ -94,6 +94,19 @@ angular
 
         return $httpProvider.responseInterceptors.push(logoutOnAccessForbidden);
     })
+    .directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngEnter);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    })
     .filter('fromTimestamp', function () {
 
         return function (timestamp) {
