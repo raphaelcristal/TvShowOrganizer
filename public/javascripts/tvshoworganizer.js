49fc558fa2542fb3b5ec645e6701567107e3c784
Raphael Brand <raphaelbrand@gmail.com>
2014-01-26 16:40:04 +0100

    collapse topbar on link selection

--- public/javascripts/tvshoworganizer.js
+++ public/javascripts/tvshoworganizer.js
@@ -493,3 +493,11 @@ angular
 
             };
         }])
+    .run(function() {
+        $(document).on('click','.navbar-collapse.in',function(e) {
+            if( $(e.target).is('a') ) {
+                $(this).collapse('hide');
+            }
+        });
+    })
+
