f6a328f13b119f100edc17fd07d8ebc9aab31787
Raphael Brand <raphaelbrand@gmail.com>
2014-02-23 12:47:30 +0100

    only run show updater in production

--- app/Global.java
+++ app/Global.java
@@ -17,27 +17,29 @@ public class Global extends GlobalSettings {
 
     @Override
     public void onStart(Application app) {
-        Akka.system().scheduler().schedule(
-                Duration.create(60, TimeUnit.SECONDS),
-                Duration.create(1, TimeUnit.DAYS),
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        TvdbProvider tvdbProvider = new TvdbProvider(Play.application().configuration().getString("tvdb.token"), TvdbProvider.Frequency.DAILY);
-                        TvdbParser tvdbParser = new TvdbParser(tvdbProvider, false);
-                        List<AbstractShowParser> parsers = new ArrayList<>();
-                        parsers.add(tvdbParser);
-                        DataLoad dataLoad = new DataLoad(parsers);
+        if (Play.isProd()) {
+            Akka.system().scheduler().schedule(
+                    Duration.create(60, TimeUnit.SECONDS),
+                    Duration.create(1, TimeUnit.DAYS),
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            TvdbProvider tvdbProvider = new TvdbProvider(Play.application().configuration().getString("tvdb.token"), TvdbProvider.Frequency.DAILY);
+                            TvdbParser tvdbParser = new TvdbParser(tvdbProvider, false);
+                            List<AbstractShowParser> parsers = new ArrayList<>();
+                            parsers.add(tvdbParser);
+                            DataLoad dataLoad = new DataLoad(parsers);
 
-                        try {
-                            dataLoad.updateShows();
-                        } catch (Exception e) {
-                            Logger.error(e.toString());
+                            try {
+                                dataLoad.updateShows();
+                            } catch (Exception e) {
+                                Logger.error(e.toString());
+                            }
                         }
-                    }
-                },
-                Akka.system().dispatcher()
-        );
+                    },
+                    Akka.system().dispatcher()
+            );
+        }
 
     }
 }
